f6d7331150a64799430d5c0de97588502a9363d7
HarHarLinks <2803622+HarHarLinks@users.noreply.github.com>
2023-10-07 17:39:38 +0200

    bump python & libolm versions

--- setup.py
+++ setup.py
@@ -53,10 +53,9 @@ setup(
     classifiers=[
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
     long_description=long_description,
     long_description_content_type="text/markdown",
