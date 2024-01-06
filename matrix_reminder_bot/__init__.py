f62e8a265c6473ed6222225d3ba5d004805b7bde
HarHarLinks <2803622+HarHarLinks@users.noreply.github.com>
2024-01-06 20:44:05 +0100

    0.3.0

--- matrix_reminder_bot/__init__.py
+++ matrix_reminder_bot/__init__.py
@@ -1,8 +1,8 @@
 import sys
 
 # Check that we're not running on an unsupported Python version.
-if sys.version_info < (3, 6):
-    print("matrix_reminder_bot requires Python 3.6 or above.")
+if sys.version_info < (3, 8):
+    print("matrix_reminder_bot requires Python 3.8 or above.")
     sys.exit(1)
 
-__version__ = "0.2.1"
+__version__ = "0.3.0"
