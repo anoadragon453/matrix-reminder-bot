139b57e64b5a5a5950969f43c62fc55016d802e7
HarHarLinks <2803622+HarHarLinks@users.noreply.github.com>
2024-01-18 10:49:39 +0100

    add python 3.12 support

--- setup.py
+++ setup.py
@@ -57,6 +57,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
     long_description=long_description,
     long_description_content_type="text/markdown",
