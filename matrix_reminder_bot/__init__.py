20bc821df77dc5c20f7264f6ef09e1753584e99b
HarHarLinks <2803622+HarHarLinks@users.noreply.github.com>
2025-10-13 10:47:09 +0200

    bump min python to 3.10, max to 3.14
    Signed-off-by: HarHarLinks <2803622+HarHarLinks@users.noreply.github.com>

--- matrix_reminder_bot/__init__.py
+++ matrix_reminder_bot/__init__.py
@@ -1,8 +1,8 @@
 import sys
 
 # Check that we're not running on an unsupported Python version.
-if sys.version_info < (3, 8):
-    print("matrix_reminder_bot requires Python 3.8 or above.")
+if sys.version_info < (3, 10):
+    print("matrix_reminder_bot requires Python 3.10 or above.")
     sys.exit(1)
 
 __version__ = "0.4.0"
